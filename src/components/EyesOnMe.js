47a5db7b5247524fdbcf8830a660af7ebc363123
Seano <sean01234@yahoo.com>
2023-06-02 19:06:15 -0400

    update Keypad and EyesOnMe

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,23 @@
 // Code EyesOnMe Component Here
+import React from "react";
+
+function EyesOnMe() {
+
+  function handleFocus(event) {
+    console.log("Good!");
+  }
+
+  function handleBlur(event) {
+    console.log("Hey! Eyes on me!");
+  }
+
+
+  return (
+    <div>
+      <button type="button" name="eyesbutton" onFocus={() => handleFocus()} onBlur={() => handleBlur()} >Eyes on me</button>
+
+    </div>
+  )
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,16 @@
 // Code Keypad Component Here
+import React from "react";
 
-function Keypad (){
-    return (
-        <div></div>
-    )
+
+function Keypad() {
+  function handleChange(event) {
+    console.log("Entering password...");
+  }
+  return (
+    <div>
+      <input type="password" name="kepadinput" onChange={handleChange} />
+    </div>
+  )
 }
 
 export default Keypad;
